38f0b6de716e126dd166d9333763bc0faf6df588
gunstr <gunnar@adalo.se>
2026-03-02 10:02:03 +0000

    Invclude the static folder in the repo for GitHub distribution to workl

---
+++ inventree_shipment_data/static/Settings.js
@@ -0,0 +1,2 @@
+const e=window.MantineCore.Text;function n(t){return React.createElement(e,null,"No custom settings for InvenTree Shipment Data.")}export{n as renderPluginSettings};
+//# sourceMappingURL=Settings.js.map
